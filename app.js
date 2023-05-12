8b0bac17268a7661f394e0ecce61299db394f537
Juan <tamez.jm@gmail.com>
2023-05-12 16:26:11 -0600

    cors

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-const express = require('express');
-const rateLimit = require('express-rate-limit');
-const ck = require('ckey');
-const morgan = require('morgan');
-const sportRouter = require('./routes/sportRoutes');
-const eventRouter = require('./routes/eventRoutes');
-const countryRouter = require('./routes/countryRoutes');
-const tournamentRouter = require('./routes/tournamentRoutes');
+const express = require("express");
+const rateLimit = require("express-rate-limit");
+const ck = require("ckey");
+const morgan = require("morgan");
+const cors = require("cors");
+const sportRouter = require("./routes/sportRoutes");
+const eventRouter = require("./routes/eventRoutes");
+const countryRouter = require("./routes/countryRoutes");
+const tournamentRouter = require("./routes/tournamentRoutes");
 
 const app = express();
+app.use(cors());
 
-if (ck.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (ck.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
@@ -18,13 +20,13 @@ app.use(express.static(`${__dirname}/public`));
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
-  message: 'Too many requests from this IP, please try again in an hour!',
+  message: "Too many requests from this IP, please try again in an hour!",
 });
-app.use('/api', limiter);
+app.use("/api", limiter);
 
-app.use('/api/v1/sports', sportRouter);
-app.use('/api/v1/events', eventRouter);
-app.use('/api/v1/country', countryRouter);
-app.use('/api/v1/tournament', tournamentRouter);
+app.use("/api/v1/sports", sportRouter);
+app.use("/api/v1/events", eventRouter);
+app.use("/api/v1/country", countryRouter);
+app.use("/api/v1/tournament", tournamentRouter);
 
 module.exports = app;
