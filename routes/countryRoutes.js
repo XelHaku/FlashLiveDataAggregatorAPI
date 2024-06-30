2c194943036860482b1222dc530bd6835b54d5de
Juan Tamez <tamez.jm@gmail.com>
2024-06-30 17:33:46 -0600

    Update countryRoutes.js

--- routes/countryRoutes.js
+++ routes/countryRoutes.js
@@ -4,6 +4,6 @@ const countryController = require("../controllers/countryController");
 const router = express.Router();
 
 router.route("/").get(countryController.getCountries);
-// router.route("/:sportId").get(countryController.getCountriesWithUpcomingcountrys);
+// router.route("/:sport").get(countryController.getCountriesWithUpcomingcountrys);
 
 module.exports = router;
