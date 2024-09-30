649e33b9433e696b0c864664a193ba69b2b61f0d
Juan Tamez <tamez.jm@gmail.com>
2024-09-30 09:58:43 -0600

    Update server.js

--- server.js
+++ server.js
@@ -55,43 +55,43 @@ process.on("unhandledRejection", (err) => {
 //   }
 // })();
 
-// Schedule tasks using node-cron
-nodeCron.schedule("0 * * * *", async () => {
-  // Runs at the top of every hour
-  try {
-    await getEvents();
-    console.log("getEvents executed successfully.");
-  } catch (err) {
-    console.error("Error executing getEvents:", err);
-  }
-});
+// // Schedule tasks using node-cron
+// nodeCron.schedule("0 * * * *", async () => {
+//   // Runs at the top of every hour
+//   try {
+//     await getEvents();
+//     console.log("getEvents executed successfully.");
+//   } catch (err) {
+//     console.error("Error executing getEvents:", err);
+//   }
+// });
 
-// Schedule tasks using node-cron, every 5 minutes
+// // Schedule tasks using node-cron, every 5 minutes
 
-nodeCron.schedule("5 * * * *", async () => {
-  // Runs at the top of every hour
-  try {
-    await callOracle();
-    console.log("getEvents executed successfully.");
-  } catch (err) {
-    console.error("Error executing getEvents:", err);
-  }
-});
+// nodeCron.schedule("5 * * * *", async () => {
+//   // Runs at the top of every hour
+//   try {
+//     await callOracle();
+//     console.log("getEvents executed successfully.");
+//   } catch (err) {
+//     console.error("Error executing getEvents:", err);
+//   }
+// });
 
-nodeCron.schedule("0 */4 * * *", async () => {
-  // Runs every 4 hours
-  try {
-    await updateUnfinishedEvents();
-    console.log("updateUnfinishedEvents executed successfully.");
-  } catch (err) {
-    console.error("Error executing updateUnfinishedEvents:", err);
-  }
-});
+// nodeCron.schedule("0 */4 * * *", async () => {
+//   // Runs every 4 hours
+//   try {
+//     await updateUnfinishedEvents();
+//     console.log("updateUnfinishedEvents executed successfully.");
+//   } catch (err) {
+//     console.error("Error executing updateUnfinishedEvents:", err);
+//   }
+// });
 
-// Graceful shutdown on SIGTERM
-process.on("SIGTERM", () => {
-  console.log("👋 SIGTERM RECEIVED. Shutting down gracefully...");
-  server.close(() => {
-    console.log("💥 Process terminated!");
-  });
-});
+// // Graceful shutdown on SIGTERM
+// process.on("SIGTERM", () => {
+//   console.log("👋 SIGTERM RECEIVED. Shutting down gracefully...");
+//   server.close(() => {
+//     console.log("💥 Process terminated!");
+//   });
+// });
